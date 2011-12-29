55728ed52a4ed555a3bbfe16974e45befca4cc4c
sujal shah <codesujal@gmail.com>
2011-12-29 01:38:27 -0500

    fixes serialization bug triggered by em-mongo
    em-mongo insists that the document ID be :_id (a Symbol), not a "_id"
    (String)
    
    Will submit issue to em-mongo to ask if they should match the standard
    ruby driver's behavior which doesn't assume.

--- lib/em-synchrony/mongoid/extras.rb
+++ lib/em-synchrony/mongoid/extras.rb
@@ -11,5 +11,16 @@ module EM::Mongo
     def insert(doc_or_docs,opts=nil)
       driver_insert(doc_or_docs)
     end
+    
+    # Mongoid expects :_id to be "_id" (String, not a Symbol) for 
+    # serialization. it looks like the mongo-ruby-driver accepts either... 
+    # and it doesn't force it to be a Symbol like em-mongo does. Probably
+    # should make the real em-mongo match this. Until then... flip
+    # sym to string by standardizing on string...
+    def sanitize_id!(doc)
+      doc["_id"] = has_id?(doc) || BSON::ObjectId.new
+      doc.delete(:_id)
+      doc
+    end
   end
 end

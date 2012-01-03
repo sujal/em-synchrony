2916e600d9ef9e714ea4d20e6fc5dc01b5b393ef
sujal shah <codesujal@gmail.com>
2012-01-03 01:14:27 -0500

    deleting unnecessary code

--- lib/em-synchrony/mongoid/database.rb
+++ lib/em-synchrony/mongoid/database.rb
@@ -9,54 +9,6 @@ module Mongoid #:nodoc:
       # keys to remove from self to not pass through to Mongo::Connection
       ASYNC_PRIVATE_OPTIONS = %w(uri database username password logger pool_timeout pool_size)
 
-      # Configure the database connections. This will return an array
-      # containing the master and an array of slaves.
-      #
-      # @example Configure the connection.
-      #   db.configure
-      #
-      # @return [ Array<Mongo::DB, Array<Mongo:DB>> ] The Mongo databases.
-      #
-      # @since 2.0.0.rc.1
-      def configure
-        [ master.db(name), slaves.map { |slave| slave.db(name) } ]
-      end
-
-      # Create the new db configuration class.
-      #
-      # @example Initialize the class.
-      #   Config::Database.new(
-      #     false, "uri" => { "mongodb://durran:password@localhost:27017/mongoid" }
-      #   )
-      #
-      # @param [ Hash ] options The configuration options.
-      #
-      # @option options [ String ] :database The database name.
-      # @option options [ String ] :host The database host.
-      # @option options [ String ] :password The password for authentication.
-      # @option options [ Integer ] :port The port for the database.
-      # @option options [ String ] :uri The uri for the database.
-      # @option options [ String ] :username The user for authentication.
-      #
-      # @since 2.0.0.rc.1
-      def initialize(options = {})
-        merge!(options)
-      end
-
-      private
-
-      # Do we need to authenticate against the database?
-      #
-      # @example Are we authenticating?
-      #   db.authenticating?
-      #
-      # @return [ true, false ] True if auth is needed, false if not.
-      #
-      # @since 2.0.0.rc.1
-      def authenticating?
-        username || password
-      end
-
       # Takes the supplied options in the hash and created a URI from them to
       # pass to the Mongo connection object.
       #
@@ -114,31 +66,6 @@ module Mongoid #:nodoc:
         end
       end
 
-      # Should we use a logger?
-      #
-      # @example Should we use a logger?
-      #   database.logger?
-      #
-      # @return [ true, false ] Defaults to true, false if specifically
-      #   defined.
-      #
-      # @since 2.2.0
-      def logger?
-        self[:logger].nil? || self[:logger] ? true : false
-      end
-
-      # Convenience for accessing the hash via dot notation.
-      #
-      # @example Access a value in alternate syntax.
-      #   db.host
-      #
-      # @return [ Object ] The value in the hash.
-      #
-      # @since 2.0.0.rc.1
-      def method_missing(name, *args, &block)
-        self[name.to_s]
-      end
-
       # Get the name of the database, from either the URI supplied or the
       # database value in the options.
       #
@@ -172,19 +99,6 @@ module Mongoid #:nodoc:
           inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo} # mongo likes symbols
       end
 
-      # Get a Mongo compliant URI for the database connection.
-      #
-      # @example Get the URI.
-      #   db.uri
-      #
-      # @param [ Hash ] options The options hash.
-      #
-      # @return [ String ] The URI for the connection.
-      #
-      # @since 2.0.0.rc.1
-      # def uri(options = {})
-      #   options["uri"] || build_uri(options)
-      # end
     end
   end
 end
